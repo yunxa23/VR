a717b3696f6f93835c7f5187e41febd2d9f2801e
yunxa23 <annayun0223@gmail.com>
2025-04-25 11:48:07 +0800

    us

---
+++ us/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "美國村",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.2484050751789297,
+          "pitch": -0.1466539158519673,
+          "title": "美國村",
+          "text": "位在沖繩中部地區北谷町的美濱，是具美國風情的地方。"
+        }
+      ]
+    }
+  ],
+  "name": "美國村",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
