e4fdb578b51bd503366a29be2b44d3c146a6b633
yunxa23 <annayun0223@gmail.com>
2025-04-25 11:42:52 +0800

    castle

---
+++ castle/data.js
@@ -0,0 +1,466 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "入口守禮之邦",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1341,
+      "initialViewParameters": {
+        "yaw": -2.244319470508323,
+        "pitch": -0.19082156426414976,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3185757004715075,
+          "pitch": -0.02383578871796388,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -1.5338238367953974,
+          "pitch": 0.11394844944673466,
+          "rotation": 0.7853981633974483,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0225436394714187,
+          "pitch": -0.337153039478137,
+          "title": "守禮之邦",
+          "text": "「守禮之邦」，意指「琉球乃為重視禮節之國」"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "歡會門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.094725184844819,
+        "pitch": -0.14198925797064277,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1313065347372904,
+          "pitch": 0.4401304866618858,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": 3.1390495557868014,
+          "pitch": 0.06674953457041433,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7933055294701727,
+          "pitch": -0.25717437561854695,
+          "title": "歡會門",
+          "text": "進入首里城的城郭內的第1正門，\"歓會\"是歡迎的意思"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "瑞泉門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -2.9243153421832258,
+        "pitch": -0.1960361762373264,
+        "fov": 1.0897717923037307
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6221032951589507,
+          "pitch": 0.2767898649905618,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.036960412814391,
+          "pitch": -0.2903541533420295,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8788900680462994,
+          "pitch": -0.31322155760810233,
+          "title": "瑞泉門",
+          "text": "第二正門，\"瑞泉\"是\"豪華的可喜的泉\"這個意思"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "漏刻門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1340.5,
+      "initialViewParameters": {
+        "yaw": -2.0639767494370194,
+        "pitch": -0.2595072215388363,
+        "fov": 1.1366317381857776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9118164160074809,
+          "pitch": 0.1827907080817539,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        },
+        {
+          "yaw": -2.312268799855124,
+          "pitch": -0.22872758075545008,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.06389401323702,
+          "pitch": -0.259424485338835,
+          "title": "漏刻門",
+          "text": "第三正門，\"漏刻\"是\"壺漏\"的意思"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "廣福門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.3176764253520723,
+        "pitch": -0.0788882060301912,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47334445922841084,
+          "pitch": 0.2851819428990332,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.5324430168239456,
+          "pitch": -0.0617488592942852,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2551856416085556,
+          "pitch": -0.16369308977585106,
+          "title": "廣福門",
+          "text": "「廣福」帶有「福運廣傳」的含意，別名「長御門」，是前往首里城正殿所經由的第四道門。"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "奉神門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8988400566094761,
+          "pitch": 0.2888734230077823,
+          "rotation": 10.210176124166829,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.5722033954738617,
+          "pitch": 0.4917962440086381,
+          "rotation": 8.63937979737193,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.37769608149426404,
+          "pitch": -0.06730340805515489,
+          "title": "奉神門",
+          "text": "為「崇敬神明之門」的意思，是進入首里城正殿前的最後一道城門"
+        }
+      ]
+    },
+    {
+      "id": "6-",
+      "name": "首里森御嶽",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.1274634640746335,
+        "pitch": 0.18318679256561765,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3183217957709594,
+          "pitch": 0.32799300295960876,
+          "rotation": 5.497787143782138,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.0566745260498323,
+          "pitch": 0.5666013137821793,
+          "rotation": 1.5707963267948966,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.535638544544355,
+          "pitch": 0.3306376540926639,
+          "title": "首里森御獄",
+          "text": "根據「琉球開闢神話」記載，首里森御嶽是神靈開闢的聖地"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "系圖座・用物座",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0917261035328103,
+        "pitch": 0.06875430724907972,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2074578810959284,
+          "pitch": 0.2219441294359381,
+          "rotation": 3.9269908169872414,
+          "target": "6-"
+        },
+        {
+          "yaw": -2.2767442520533088,
+          "pitch": 0.07763604366165033,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.053049604786871,
+          "pitch": -0.033075393102123485,
+          "title": "系圖座・用物座",
+          "text": "「系圖座」是管理士族家譜的機關，「用物座」則是管理首里城內使用的物品或材料等機關"
+        }
+      ]
+    },
+    {
+      "id": "8-",
+      "name": "首里社館",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.943951965809058,
+        "pitch": -0.1526556604713356,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9663268242624365,
+          "pitch": 0.3351306416471651,
+          "rotation": 3.141592653589793,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.605473092764811,
+          "pitch": -0.050184184940050613,
+          "rotation": 0.7853981633974483,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.487238009680425,
+          "pitch": -0.05755466647847207,
+          "title": "首里社館",
+          "text": "首里城公園的綜合服務中心，提供遊客資訊展覽及休憩的場所"
+        }
+      ]
+    }
+  ],
+  "name": "首里城",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
